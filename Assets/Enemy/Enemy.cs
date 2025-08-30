89169905dc80625f6936c6cac18ca35b0fce85ad
Muhtadin Ziqi Maulana <muhtadinzm@gmail.com>
2025-08-30 14:58:36 +0700

    Implement patrol state enemy

--- Assets/Enemy/Enemy.cs
+++ Assets/Enemy/Enemy.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
@@ -9,15 +11,22 @@ public class Enemy : MonoBehaviour
     public ChaseState ChaseState = new ChaseState();
     public RetreatState RetreatState = new RetreatState();
 
+    [SerializeField]
+    public List<Transform> Waypoints = new List<Transform>();
+
+    [HideInInspector]
+    public UnityEngine.AI.NavMeshAgent NavMeshAgent;
+
     private void Awake()
     {
         _currentState = PatrolState;
         _currentState.EnterState(this);
+        NavMeshAgent = GetComponent<NavMeshAgent>();
     }
 
     private void Update()
     {
-        if(_currentState != null)
+        if (_currentState != null)
         {
             _currentState.UpdateState(this);
         }
