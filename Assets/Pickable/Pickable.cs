69499250d1ef34bd3c342aac3d61ed03b38bfabd
Muhtadin Ziqi Maulana <muhtadinzm@gmail.com>
2025-08-30 12:09:53 +0700

    Implement Pickable

---
+++ Assets/Pickable/Pickable.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickable : MonoBehaviour
+{
+
+    [SerializeField]
+    public PickableType PickableType;
+
+    public Action<Pickable> OnPicked;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if(OnPicked != null)
+            {
+                OnPicked(this);
+            }
+        }
+    }
+}
