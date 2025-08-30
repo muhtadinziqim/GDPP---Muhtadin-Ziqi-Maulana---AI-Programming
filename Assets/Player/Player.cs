929e3aa249c6b914f73504dcea974cfb3d492d25
Muhtadin Ziqi Maulana <muhtadinzm@gmail.com>
2025-08-30 11:06:25 +0700

    Movement Player

---
+++ Assets/Player/Player.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody _rigidBody;
+
+    [SerializeField]
+    private float _speed;
+
+    private void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 movementDirection = new Vector3(horizontal,0,vertical);
+        _rigidBody.linearVelocity = movementDirection * _speed * Time.fixedDeltaTime;
+    }
+}
